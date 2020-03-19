e7102e26075b6caed8f008d548e029a8c0037db4
uiuihby <1350285537@qq.com>
2020-03-19 22:38:57 +0800

    ~

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 var request = require('superagent')
 var fs = require('fs');
-const cheerio = require('cheerio');
-let cookie="SINAGLOBAL=3133917307893.437.1584368319827; UOR=www.baidu.com,weibo.com,login.sina.com.cn; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFuxeZTWRm0rpHAzbvI359.5JpX5KMhUgL.Foqpeh.cehn7ShB2dJLoI7pHCHHiC2i91hncSo2t; ALF=1616071175; SSOLoginState=1584535176; SCF=AnfJOvYotmTdP0O3DQU7V1f05nx-lNSo06D3o2EYwxL0lhs9jOtisGPer1Ea0wB0JvFDlnLKI-hIu4O55rD6obs.; SUB=_2A25zdmbYDeRhGeBP61sX8CbMzziIHXVQAt8QrDV8PUNbmtAKLVCtkW9NRZNPoaDRnLRJZMazXcYQk9LcopAkTxYD; SUHB=0wnlQDj84w5tMA; Ugrow-G0=5c7144e56a57a456abed1d1511ad79e8; wb_view_log_6109608044=1920*10801; _s_tentry=login.sina.com.cn; Apache=6102895483411.044.1584535175292; ULV=1584535175313:4:4:4:6102895483411.044.1584535175292:1584448653010; TC-V5-G0=595b7637c272b28fccec3e9d529f251a; webim_unReadCount=%7B%22time%22%3A1584535361056%2C%22dm_pub_total%22%3A1%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A6%2C%22msgbox%22%3A0%7D; TC-Page-G0=2f200ef68557e15c78db077758a88e1f|1584535385|1584535174"
 
-    let arr=""
+var http = require('http');
+var https = require('https');
+const cheerio = require('cheerio');
+let cookie="SINAGLOBAL=3133917307893.437.1584368319827; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFuxeZTWRm0rpHAzbvI359.5JpX5KMhUgL.Foqpeh.cehn7ShB2dJLoI7pHCHHiC2i91hncSo2t; ALF=1616157279; SSOLoginState=1584621283; SCF=AnfJOvYotmTdP0O3DQU7V1f05nx-lNSo06D3o2EYwxL0G9VcBFcMiZnlzeWX_Jd6JfVySxxbYybLOjxBldXWNgE.; SUB=_2A25zdxazDeRhGeBP61sX8CbMzziIHXVQBQ97rDV8PUNbmtAKLUrNkW9NRZNPoULm7OeNOaUgBked78ygEAmDUHBy; SUHB=0FQ7je8QJi2K-e; _s_tentry=login.sina.com.cn; Apache=4008640267094.8384.1584621277729; ULV=1584621277741:5:5:5:4008640267094.8384.1584621277729:1584535175313; UOR=www.baidu.com,weibo.com,www.baidu.com; Ugrow-G0=589da022062e21d675f389ce54f2eae7; TC-V5-G0=799b73639653e51a6d82fb007f218b2f; wb_view_log_6109608044=1920*10801; TC-Page-G0=b32a5183aa64e96302acd8febeb88ce4|1584621982|1584621924; webim_unReadCount=%7B%22time%22%3A1584622013600%2C%22dm_pub_total%22%3A1%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A12%2C%22msgbox%22%3A0%7D"
+
+var express = require("express");
+const path = require('path')
+var app = express();
+// 各种依赖
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use(express.static("./public"));
+app.get("/news",function(req,res){
+	 console.log(req)
+	 console.log(res)
+});
+app.listen(8888);
+ 
+// 终端打印如下信息   每次更新都要重新启动 快捷键cart+c   运行node xxx.js
+console.log('Server running at http://127.0.0.1:8888/');
+return 
+
+
+let htmlTop
+
+
+
+  request
+  .get("https://m.weibo.cn/statuses/show?id=EywUqmYFi")
+  .set('Cookie', cookie)
+  .then(res => {
+     let pic  = "<div id='titlePic'>" +res.body.data.thumbnail_pic+ "</div>"
+     let text = "<div id='titleMsg'>" +res.body.data.text+ "</div>"
+     arr+=pic
+     arr+=text
+  })
+  .catch(err => {
+     console.log(err);
+  });
+
+   let arr=""
   function getFirstCom(url){
     request
     .get(url)
@@ -57,13 +99,21 @@ let cookie="SINAGLOBAL=3133917307893.437.1584368319827; UOR=www.baidu.com,weibo.
   getSecCom("https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4082296585476416&root_comment_max_id_type=0&root_comment_ext_param=&page=1","https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4082296585476416&root_comment_max_id_type=0&root_comment_ext_param=&page=")
 
   setTimeout(function(){
-    let html='<!DOCTYPE html><html lang="en"><body>'+arr+'</body></html>'
-    
-    //  fs.writeFileSync('test.html',html);
-      html = html.toString();
+    let html
+    html+='<!DOCTYPE html><html lang="en"><body>'+arr+'</body></html>'
+ 
+      // fs.writeFileSync('test3.html',html);
 
-     let $ = cheerio.load(html);
+      
 
-     $('body').find('.list_box').text()
 
   },5000)
+
+  app.post('/getData', function (req, res) {  //  新增
+   var result = req.body
+   console.log(result)
+
+})
+
+
+
